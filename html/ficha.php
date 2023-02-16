377af3210fefd23026271c4176d4397f90ff4c3a
Wadson Pontes <v007012@intrajus.tjrn>
2023-02-16 16:57:17 -0300

    Criando script da ficha

--- html/ficha.php
+++ html/ficha.php
@@ -1,76 +1,113 @@
-<section id="ficha" class="fechado">
+<section id="tela-ficha" class="fechado">
     <table>
         <tr>
             <th>Energia</th>
-            <td class="energia"></td>
+            <td id="energia"></td>
         </tr>
         <tr>
             <th>Vida</th>
-            <td class="vida"></td>
+            <td id="vida"></td>
         </tr>
         <tr>
             <th>Dano</th>
-            <td class="dano"></td>
+            <td id="dano"></td>
         </tr>
         <tr>
             <th>Chance de Crítico</th>
-            <td class="chance"></td>
+            <td id="chance"></td>
         </tr>
         <tr>
             <th>Armadura</th>
-            <td class="armadura"></td>
+            <td id="armadura"></td>
         </tr>
         <tr>
             <th>Reflexo</th>
-            <td class="reflexo"></td>
+            <td id="reflexo"></td>
         </tr>
         <tr>
             <th>Velocidade</th>
-            <td class="velocidade"></td>
+            <td id="velocidade"></td>
         </tr>
     </table>
 
-    <h3>Pontos para distribuir: <span class="pontos">0</span></h3>
+    <h3>Pontos para distribuir: <span id="pontos">0</span></h3>
 
     <table>
         <tr>
             <th>Vigor</th>
-            <td></td>
+            <td id="vigor"></td>
             <td><button>+</button></td>
         </tr>
         <tr>
             <th>Vitalidade</th>
-            <td></td>
+            <td id="vitalidade"></td>
             <td><button>+</button></td>
         </tr>
         <tr>
             <th>Força</th>
-            <td></td>
+            <td id="forca"></td>
             <td><button>+</button></td>
         </tr>
         <tr>
             <th>Sorte</th>
-            <td></td>
+            <td id="sorte"></td>
             <td><button>+</button></td>
         </tr>
         <tr>
             <th>Resistência</th>
-            <td></td>
+            <td id="resistencia"></td>
             <td><button>+</button></td>
         </tr>
         <tr>
             <th>Destreza</th>
-            <td></td>
+            <td id="destreza"></td>
             <td><button>+</button></td>
         </tr>
         <tr>
             <th>Agilidade</th>
-            <td></td>
+            <td id="agilidade"></td>
             <td><button>+</button></td>
         </tr>
     </table>
 </section>
 
 <script>
-    const energia = document.queryElement('energia');
+    const tela_ficha = document.querySelector('#tela-ficha');
+    const energia = document.queryElement('#energia');
+    const vida = document.queryElement('#vida');
+    const dano = document.queryElement('#dano');
+    const chance = document.queryElement('#chance');
+    const armadura = document.queryElement('#armadura');
+    const reflexo = document.queryElement('#reflexo');
+    const velocidade = document.queryElement('#velocidade');
+    const pontos = document.queryElement('#pontos');
+    const vigor = document.queryElement('#vigor');
+    const vitalidade = document.queryElement('#vitalidade');
+    const forca = document.queryElement('#forca');
+    const sorte = document.queryElement('#sorte');
+    const resistencia = document.queryElement('#resistencia');
+    const destreza = document.queryElement('#destreza');
+    const agilidade = document.queryElement('#agilidade');
+
+    function ficha() {
+        tela_ficha.classList.remove('fechado');
+        tela_atual = tela_ficha;
+        let resposta = get('ficha');
+
+        energia.textContent = resposta.energia;
+        vida.textContent = resposta.vida;
+        dano.textContent = resposta.dano;
+        chance.textContent = resposta.chance;
+        armadura.textContent = resposta.armadura;
+        reflexo.textContent = resposta.reflexo;
+        velocidade.textContent = resposta.velocidade;
+        pontos.textContent = resposta.pontos;
+        vigor.textContent = resposta.vigor;
+        vitalidade.textContent = resposta.vitalidade;
+        forca.textContent = resposta.forca;
+        sorte.textContent = resposta.sorte;
+        resistencia.textContent = resposta.resistencia;
+        destreza.textContent = resposta.destreza;
+        agilidade.textContent = resposta.agilidade;
+    }
 </script>
