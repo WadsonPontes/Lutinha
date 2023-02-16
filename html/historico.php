2b5a02768416478948327510c911e7957f9782e2
Wadson Pontes <v007012@intrajus.tjrn>
2023-02-16 11:02:28 -0300

    Atualização

--- html/historico.php
+++ html/historico.php
@@ -22,35 +22,8 @@
     const paginacao_historico = document.querySelector('#paginacao-historico');
 
     function historico(inicial = 1, quantidade = 10) {
-        let historicos = get('historico', inicial, quantidade);
-
         tela_historico.classList.remove('fechado');
         tela_atual = tela_historico;
-        tabela_historico.innerHTML = '';
-
-        for (let luta of historicos.lutas) {
-            const tr_linha = document.createElement('tr');
-            const td_resultado = document.createElement('td');
-            const td_nome = document.createElement('td');
-            const td_nivel = document.createElement('td');
-            const td_pontos = document.createElement('td');
-            const td_data = document.createElement('td');
-
-            td_resultado.textContent = luta.resultado;
-            td_nome.textContent = luta.nome;
-            td_nivel.textContent = luta.nivel;
-            td_pontos.textContent = luta.pontos;
-            td_data.textContent = luta.data;
-
-            tr_linha.appendChild(td_resultado);
-            tr_linha.appendChild(td_nome);
-            tr_linha.appendChild(td_nivel);
-            tr_linha.appendChild(td_pontos);
-            tr_linha.appendChild(td_data);
-
-            tabela_historico.appendChild(tr_linha);
-        }
-
-        paginacao(historico, paginacao_historico, inicial, quantidade, historicos.total);
+        attTabela('historico', tabela_historico, paginacao_historico, inicial, quantidade);
     }
 </script>
--- js/api.js
+++ js/api.js
@@ -1,19 +1,7 @@
 function get(tabela, ...args) {
     return {
-        
-    };
-}
 
-function addBotaoPagina(d) {
-    if (d.i == 0) criar(d.pai, 'button', '<<', false, d.clique, 1, d.quantidade);
-    else if (d.i == 1) criar(d.pai, 'button', '<', false, d.clique, Math.max(1, d.inicial - d.quantidade), d.quantidade);
-    else if (d.i == 2) criar(d.pai, 'button', '1', false, d.clique, 1, d.quantidade);
-    else if (d.i == 3 && pagina(d)) criar(d.pai, 'button', '...', true);
-    else if (d.i == 9 && pagina(d)) criar(d.pai, 'button', '...', true);
-    else if (d.i ==10 && pagina(d)) criar(d.pai, 'button', '...', true);
-    else if (d.i ==11) criar(d.pai, 'button', '>', false, d.clique, Math.min(ultimo(d), d.inicial + d.quantidade), d.quantidade);
-    else if (d.i ==12) criar(d.pai, 'button', '>>', false, d.clique, ultimo(d), d.quantidade);
-    else criar(d.pai, 'button', pagina(d.i), false, d.clique, primeiro(d.i), d.quantidade);
+    };
 }
 
 function pagina(d) {
@@ -34,65 +22,51 @@ function ultimo(d) {
 function criar(pai, tipo, texto, desativado, clique, ...parametros) {
     const elemento = document.createElement(tipo);
     elemento.textContent = texto;
-    elemento.disabled = desativado;
-    elemento.onclick = () => clique(...parametros);
+    if (tipo == 'button') elemento.disabled = desativado;
+    if (clique) elemento.onclick = () => clique(...parametros);
     pai.appendChild(elemento);
 }
 
-function paginacao(clique, pai, inicial, quantidade, total) {
+function criarPagina(d, texto, inicial_ou_desativado = false) {
+    if (inicial_ou_desativado)
+        criar(d.pagicacao, 'button', texto, desativado, attTabela, d.nometabela, d.tabela, d.paginacao, inicial_ou_desativado, d.quantidade);
+    else
+        criar(d.pagicacao, 'button', texto, true);
+}
+
+function addPagina(d) {
+    if (d.i == 0) criarPagina(d, '<<', 1);
+    else if (d.i == 1) criar(d, '<', Math.max(1, d.inicial - d.quantidade));
+    else if (d.i == 2) criar(d, '1', 1);
+    else if (d.i == 3 && pagina(d)) criar(d, '...');
+    else if (d.i == 9 && pagina(d)) criar(d.pai, 'button', '...', true);
+    else if (d.i ==10 && pagina(d)) criar(d.pai, 'button', pagina(d.i), false, attTabela, d.nometabela, ultimo(d), d.quantidade);
+    else if (d.i ==11) criar(d.pai, 'button', '>', false, attTabela, d.nometabela, Math.min(ultimo(d), d.inicial + d.quantidade), d.quantidade);
+    else if (d.i ==12) criar(d.pai, 'button', '>>', false, attTabela, d.nometabela, ultimo(d), d.quantidade);
+    else criar(d.pai, 'button', pagina(d.i), false, attTabela, d.nometabela, primeiro(d.i), d.quantidade);
+}
+
+function paginar(nometabela, tabela, pagicacao, inicial, quantidade, total) {
     // << < 1 ... 5 6 |7| 8 9 ... 15 > >>
     for (let i = 0; i < 13; ++i) {
-        addBotaoPagina({clique, pai, inicial, quantidade, total, i});
+        addPagina({nometabela, tabela, pagicacao, inicial, quantidade, total, i});
+    }
+}
+
+function attTabela(nometabela, tabela, pagicacao, inicial = 1, quantidade = 10) {
+    let resposta = get(nometabela, inicial, quantidade);
+
+    tabela.innerHTML = '';
+
+    for (let linha of resposta.dados) {
+        const tr = document.createElement('tr');
+
+        for (let campo in linha) {
+            criar(tr, 'td', linha[campo]);
+        }
+
+        tabela.appendChild(tr);
     }
 
-    const botao_voltar_tudo = document.createElement('button');
-    botao_voltar_tudo.textContent = '<<';
-    botao_voltar_tudo.onclick = () => funcao(1, quantidade);
-
-    const botao_voltar = document.createElement('button');
-    botao_voltar.textContent = '<';
-    botao_voltar.onclick = () => funcao(Math.max(1, inicial - quantidade), quantidade);
-
-    const botao_1 = document.createElement('button');
-    botao_1.textContent = '1';
-    botao_1.onclick = () => funcao(1, quantidade);
-
-    const botao_reticencias_esquerdo = null;
-    const primeiro = Math.max(1, inicial - 2 * quantidade);
-    const botao_primeiro = null;
-    const valor_primeiro = null;
-
-    if (primeiro > 1 && primeiro <= total) {
-        botao_reticencias_esquerdo = document.createElement('button');
-        botao_reticencias_esquerdo.textContent = '...';
-        botao_reticencias_esquerdo.disabled = true;
-
-        botao_primeiro = document.createElement('button');
-        valor_primeiro = (primeiro + quantidade - 1) / Math.max(1, quantidade);
-        botao_primeiro.textContent = valor_primeiro.toString();
-        botao_primeiro.onclick = () => funcao(valor_primeiro, quantidade);
-    }
-
-    const segundo = Math.min(total, inicial - 1 * quantidade);
-    const botao_segundo = null;
-    const valor_segundo = null;
-
-    if (segundo > 1 && segundo <= total) {
-        botao_segundo = document.createElement('button');
-        valor_segundo = (segundo + quantidade - 1) / Math.max(1, quantidade);
-        botao_segundo.textContent = valor_segundo.toString();
-        botao_segundo.onclick = () => funcao(valor_segundo, quantidade);
-    }
-
-    const botao_inicial = null;
-    const valor_inicial = null;
-
-    if (inicial > 1 && inicial <= total) {
-        botao_inicial = document.createElement('button');
-        valor_inicial = (inicial + quantidade - 1) / Math.max(1, quantidade);
-        botao_inicial.textContent = valor_inicial.toString();
-        botao_inicial.onclick = () => funcao(valor_inicial, quantidade);
-    }
-
-
+    paginar(nometabela, tabela, pagicacao, inicial, quantidade, resposta.total);
 }
