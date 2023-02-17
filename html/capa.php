ae6567f83c87b336f1e98b56fed2b4ab275be28a
Wadson Pontes <v007012@intrajus.tjrn>
2023-02-17 13:23:02 -0300

    Atualizado capa

--- html/capa.php
+++ html/capa.php
@@ -1,14 +1,43 @@
-<section class="">
-    <button>ENTRAR</button>
-    <img id="cabelo" src="circulo">
-    <img id="rosto" src="circulo">
-    <img id="olhos" src="circulo">
-    <img id="pescoco" src="circulo">
-    <img id="peitoral" src="circulo">
-    <img id="braco-esquerdo" src="circulo">
-    <img id="braco-direito" src="circulo">
-    <img id="quadril" src="circulo">
-    <img id="perna-esquerda" src="circulo">
-    <img id="perna-direita" src="circulo">
-    <button>JOGAR</button>
+<section id="capa" class="">
+    <h1>Lutinha</h1>
+
+    <h2>Carregar jogo salvo</h2>
+    <input id="email-usuario" type="text" placeholder="Email ou Usuário">
+    <input id="senha" type="text" placeholder="Senha">
+    <button onclick="entrar()">ENTRAR</button>
+
+    <h2>Novo jogo</h2>
+
+    <img id="img-cabelo" src="circulo.png">
+    <select>
+        <option>Liso</option>
+        <option>Crespo</option>
+    </select>
+    <select>
+        <option>Curto</option>
+        <option>Médio</option>
+        <option>Grande</option>
+    </select>
+    <input id="cor-cabelo" type="color">
+
+    <img id="rosto" src="circulo.png">
+    <img id="olhos" src="circulo.png">
+    <img id="nariz" src="circulo.png">
+    <img id="boca" src="circulo.png">
+    <img id="pescoco" src="circulo.png">
+    <img id="peitoral" src="circulo.png">
+    <img id="braco-esquerdo" src="circulo.png">
+    <img id="braco-direito" src="circulo.png">
+    <img id="quadril" src="circulo.png">
+    <img id="perna-esquerda" src="circulo.png">
+    <img id="perna-direita" src="circulo.png">
+    <button>VOLTAR</button>
+    <button>PADRÃO</button>
+    <button>RANDOMIZAR</button>
+    <button>PRÓXIMO</button>
+    <button class="fechado">JOGAR</button>
 </section>
+
+<script>
+
+</script>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-    <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link rel="stylesheet" type="text/css" href="css/index.css">
         <link rel="stylesheet" type="text/css" href="css/capa.css">
         <script src="js/api.js"></script>
     </head>
